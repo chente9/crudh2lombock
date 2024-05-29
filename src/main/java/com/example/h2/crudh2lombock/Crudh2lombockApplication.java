724f20af2b3f4a61676035f6dc53ef599d131db3
Marcos Chuy <Vicente194591@gmail.com>
2024-05-28 19:46:00 -0600

    excercise

---
+++ src/main/java/com/example/h2/crudh2lombock/Crudh2lombockApplication.java
@@ -0,0 +1,13 @@
+package com.example.h2.crudh2lombock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crudh2lombockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crudh2lombockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/h2/crudh2lombock/Crudh2lombockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.h2.crudh2lombock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Crudh2lombockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
